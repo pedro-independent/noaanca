812fa3b0a16e901be4d7c7d6516146eca43d6f9c
Nevvees <pedro@independent.pt>
2025-04-18 16:47:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ gsap.to(".home-hero-image-wrap", {
 }
   
 /* Menu Services */
-
+if (window.innerWidth > 991) {
 let dropdownMenu;
 
 $(document).ready(function() {
@@ -88,73 +88,7 @@ $(document).ready(function() {
 
     }, 1000);
 });
-
-
-/* Hamburguer Menu Icon */
-
-document.addEventListener("DOMContentLoaded", () =>{
-
-  gsap.registerPlugin(CustomEase)
-  CustomEase.create("button-ease", "0.5, 0.05, 0.05, 0.99")
-  
-  initMenuButton()
-  
-})
-
-function initMenuButton() {
-  // Select elements
-  const menuButton = document.querySelector("[data-menu-button]");
-  const lines = document.querySelectorAll(".menu-button-line");
-  const [line1, line2, line3] = lines;
-  
-  // Define one global timeline
-  let menuButtonTl = gsap.timeline({
-    defaults:{
-      overwrite:"auto",
-      ease: "button-ease",
-  	  duration: 0.3
-    }
-  })
-
-  const menuOpen = () => {
-    menuButtonTl.clear() // Stop any previous tweens, if any
-    .to(line2, { scaleX: 0, opacity: 0 }) // Step 1: Hide middle line
-    .to(line1, { x: "-1.3em", opacity: 0 }, "<") // Step 1: Movetop line
-    .to(line3, { x: "1.3em", opacity: 0 }, "<") // Step 1: Move bottom line
-    .to([line1,line3],{opacity:0, duration: 0.1},"<+=0.2") // Step 2: Quickly fade top and bottom lines
-    .set(line1, { rotate: -135, y: "-1.3em", scaleX: 0.9 }) // Step 3: Instantly rotate and scale top line
-    .set(line3, { rotate: 135, y: "-1.4em", scaleX: 0.9 }, "<") // Step 3: Instantly rotate and scale bottom line
-    .to(line1, { opacity: 1, x: "0em", y: "0.5em"}) // Step 4: Move top line to final position
-    .to(line3, { opacity: 1, x: "0em", y: "-0.25em" }, "<+=0.1"); // Step 4: Move bottom line to final position
-  }
-
-  const menuClose = () => {
-    menuButtonTl.clear() // Stop any previous tweens, if any
-    .to([line1, line2, line3], { // Move all lines back in a different animation
-      scaleX: 1,
-      rotate: 0,
-      x: "0em",
-      y: "0em",
-      opacity: 1,
-      duration: 0.45,
-      overwrite: "auto",
-    })
-  }
-
-  // Toggle Animation
-  menuButton.addEventListener("click", () => {
-    const currentState = menuButton.getAttribute("data-menu-button");
-
-    if (currentState === "burger") {
-      menuOpen()
-      menuButton.setAttribute("data-menu-button", "close");
-    } else {
-      menuClose()
-      menuButton.setAttribute("data-menu-button", "burger");
-    }
-  });
 }
-
 
 /* Services */
 
